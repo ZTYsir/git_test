d9f96a6be2be39c7f994ea8343defd86fcde331e
ZTYsir <974808867@qq.com>
2024-07-18 14:25:08 -0700

    add mainA mainB

--- mainB.cpp
+++ mainB.cpp
@@ -4,17 +4,6 @@
 #include<chrono>
 #include<thread>
 
-
-voiud show(){
-	std::cout<<"good better best"<<std::endl;
-}
-
-void swap(int& a,int& b){
-	int tmp=a;
-	a=b;
-	b=tmp;
-}
-
 int main(){
 	std::cout<<"helloo world"<<endl;
 	return 0;
